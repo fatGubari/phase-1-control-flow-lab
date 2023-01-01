d3f68ad82ecefee7d01af889303712dbe1a0cd70
fatima abdullah <fatimajubary@gmail.com>
2023-01-01 22:33:42 +0300

    completed assignemnt

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
+  if (number===199) {
+    return "This one is on me!";
+  } else if(number>=400 && number<=2000){
+    return 'That will be twenty bucks.';
+  }else if(number<=2500){
+    return 'I will gladly take your thirty bucks.';
+  }else{
+    return 'No can do.';
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city==="NYC") {
+    return 'Ok, sounds good.';
+  } else{
+    return 'No go.';
+  }
+  // if(city==='Pittsburgh')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip==='generous') {
+    return 'Thank you so much.';
+  } else if(tip==='not as generous'){
+    return 'Thank you.'
+  }else{
+    return 'Bye.';
+  }
 }
